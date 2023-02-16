b343087ef7e3fce00e822f013495d77a975d769c
Jorge Palacios <jorge.p.codes@gmail.com>
2023-02-16 15:25:17 -0300

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const imageUrl = require('./assets/img/goku.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Jorge Palacios</Text>
+      <Text style={styles.h1}>Hola Coder!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={imageUrl} style={styles.image} />
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +19,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#130f40',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: '50%',
+  },
+  text: {
+    color: '#dff9fb',
+    fontSize: 20,
+  },
+  h1: {
+    color: '#f0932b',
+    fontSize: 40,
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  image: {
+    width: 280,
+    height: 300,
+    borderRadius: 20,
   },
 });
